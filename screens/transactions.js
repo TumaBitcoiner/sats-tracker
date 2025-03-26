8c57ab5fd53fc05d793172920c9de86b0a874ffe
TumaBitcoiner <tumathebitcoiner@gmail.com>
2025-03-26 15:51:05 +0100

    First styling of transaction card.

--- screens/transactions.js
+++ screens/transactions.js
@@ -1,5 +1,5 @@
-import React from 'react'
-import { StyleSheet, View, Text, TouchableOpacity } from 'react-native'
+import React, {useState} from 'react'
+import { StyleSheet, View, Text, TouchableOpacity, FlatList} from 'react-native'
 import { useNavigation } from "@react-navigation/native";
 import Card from '../shared/card'
 
@@ -8,13 +8,40 @@ export default function Transactions(){
 
     const navigation = useNavigation();
 
+    const [transactions, setTransactions] = useState([
+        {amount:'120', category:'Food', note:'lorem ipsum', key:'1'},
+    ]);
+
     return(
         <View>
-            <TouchableOpacity onPress={()=> navigation.navigate('TransactionDetails')}>
-                <Card>
-                    <Text>Transaction Screen</Text>
-                </Card>
-            </TouchableOpacity>
+            <FlatList 
+                data={transactions}
+                renderItem={( {item} ) => (
+                    <TouchableOpacity onPress={()=> navigation.navigate('TransactionDetails', item)}>
+                        <Card>
+                            <View style={styles.transactionCard}>
+                                <Text style={styles.transactionCategory}>{item.category}</Text>
+                                <Text style={styles.transactionAmount}>{item.amount}</Text>
+                            </View>
+                        </Card>
+                    </TouchableOpacity>
+                )}
+            />
         </View>
     )    
 }
+
+const styles = StyleSheet.create({
+
+    transactionCard:{
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+    },
+    transactionAmount: {
+        fontSize: 20,
+    },
+    transactionCategory: {
+        fontWeight: 'bold',
+        fontSize: 20,
+    }
+});
