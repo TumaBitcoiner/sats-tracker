f952f0541ce9504802abe598ef89b37686d17994
TumaBitcoiner <tumathebitcoiner@gmail.com>
2025-03-26 13:41:48 +0100

    Added transactionStack to bottom navigation

--- routes/bottomTabNavigatorHome.js
+++ routes/bottomTabNavigatorHome.js
@@ -1,9 +1,11 @@
-import { createStaticNavigation } from '@react-navigation/native';
+import { createStaticNavigation, TabRouter } from '@react-navigation/native';
 //import { Button } from '@react-navigation/elements';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import Home from '../screens/home';
 import Budget from '../screens/budget';
-import Transactions from '../screens/transactions';
+//import Transactions from '../screens/transactions';
+import TransactionStack from './transactionStack';
+import {MaterialIcons} from '@expo/vector-icons'
 
 
   const BottomTabNavigatorHome = createBottomTabNavigator({
@@ -11,15 +13,34 @@ import Transactions from '../screens/transactions';
     screens:{
         Home: {
             screen: Home,
+            options:{
+                tabBarIcon:({ color, size }) =>           
+                    <MaterialIcons name='home' color={color} size={size}/>           
+                
+            }
         },
         
-        Transactions: {
-            screen: Transactions,
+        TransactionStack: {
+            screen: TransactionStack,
+            options:{
+                tabBarIcon:({ color, size }) =>
+                    <MaterialIcons name='money' color={color} size={size} />           
+                
+            }
         }, 
         
         Budget: {
             screen: Budget,
+            options:{
+                tabBarIcon:({ color, size }) =>
+                    <MaterialIcons name='account-balance-wallet' color={color} size={size}/>           
+            }
         },
+    },
+    screenOptions:{
+        tabBarInactiveTintColor: 'black',
+        tabBarActiveTintColor: 'black',
+        tabBarInactiveBackgroundColor: 'orange',
     }
   })
 
