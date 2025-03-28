d7a95a828efdfbe18a4b89e7bdf57eac1b078b97
TumaBitcoiner <tumathebitcoiner@gmail.com>
2025-03-28 15:41:10 +0100

    Styled ButtonAdd

--- shared/buttonAdd.js
+++ shared/buttonAdd.js
@@ -6,15 +6,27 @@ export default function ButtonAdd({onPress}){
 
     return(
         <TouchableOpacity style={styles.button}>
-            <MaterialIcons name='add' size= {50} color='black' onPress={onPress}/>
+            <MaterialIcons name='add' size= {45} color='black' onPress={onPress}/>
         </TouchableOpacity>
     )
 }
 
 const styles = StyleSheet.create({
 
-    button:{        
+    button:{     
+        height: 60,
+        width: 60,   
         alignItems: 'center',
         justifyContent: 'center',
+        position: 'absolute',
+        bottom: 20,
+        alignSelf: 'center',
+        backgroundColor: 'orange',
+        borderRadius: 30,
+        elevation: 5,
+        shadowColor: '#000',
+        shadowOffset: { width: 0, height: 2 },
+        shadowOpacity: 0.3,
+        shadowRadius: 3,
     }
 })
