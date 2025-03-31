83e6c3c574415a666452372a92f20e2463e5b9d8
TumaBitcoiner <tumathebitcoiner@gmail.com>
2025-03-31 18:49:42 +0200

    Created simple flat button.

---
+++ shared/butttonFlat.js
@@ -0,0 +1,29 @@
+import React from "react";
+import {StyleSheet, View, Text, TouchableOpacity} from 'react-native';
+
+export default function ButtonFlat({ title, onPress }) {
+
+    return(
+        <TouchableOpacity onPress={onPress}>
+            <View style={styles.button}>
+                <Text style={styles.buttonText}>{title}</Text>
+            </View>
+        </TouchableOpacity>
+    )
+}
+
+const styles = StyleSheet.create({
+    button:{
+        borderRadius: 8,
+        paddingVertical: 20,
+        paddingHorizontal: 10,
+        backgroundColor: 'orange'
+    },
+    buttonText:{
+        color: 'white',
+        fontWeight: 'bold',
+        textTransform: 'uppercase',
+        fontSize: 16,
+        textAlign: 'center'
+    }
+})
