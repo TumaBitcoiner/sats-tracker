3cd9702c43ffb1596a8b787e00368f87b26ddf6d
TumaBitcoiner <tumathebitcoiner@gmail.com>
2025-04-08 11:53:22 +0200

    Basic styling of the Home screen.

--- screens/home.js
+++ screens/home.js
@@ -14,14 +14,71 @@ export default function Home(){
 
     return(
         <View>
-            <Card>               
-                <Text>Your Balance: {availableBalance}</Text>
+            <Card> 
+                <View style={styles.balanceContainer}>
+                    <Text style={styles.balanceTitle}>Your Balance</Text>
+                    <Text style={styles.balance}>{availableBalance} sats</Text>
+                </View>             
             </Card>
-            <Card>               
-                <Text>Income: {totalIncome}</Text>
-                <Text>Expenses: {totalExpenses}</Text>
-                <Text>Fees: {totalFees}</Text>
+            <Card>
+                <View>
+                    <View style={styles.balanceContainer}>
+                        <Text style={styles.amountTitle}>Balance Info</Text>
+                    </View>
+                    <Card>   
+                        <View style={styles.amountContainer}>
+                            <Text style={styles.amount}>Income:</Text>
+                            <Text style={styles.amount}>{totalIncome}</Text>
+                        </View>            
+                    </Card>
+                    <Card>                   
+                        <View style={styles.amountContainer}>
+                            <Text style={styles.amount}>Expenses:</Text>
+                            <Text style={styles.amount}>{totalExpenses}</Text>
+                        </View>                      
+                    </Card>
+                    <Card>  
+                        <View style={styles.amountContainer}>
+                            <Text style={styles.amount}>Fees Paid:</Text>
+                            <Text style={styles.amount}>{totalFees}</Text>
+                        </View>          
+                    </Card>
+                </View>
             </Card>
+            
         </View>
     )    
 }
+
+const styles = StyleSheet.create({
+
+    balanceContainer:{
+        alignContent: 'center',
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+    balanceTitle:{
+        
+        fontSize: 20,
+        fontWeight: 'bold',
+    },
+    balance:{
+        fontSize: 24,
+        color: 'green',
+        fontWeight: 'bold',
+    },
+    amount:{
+        fontSize: 15
+    },
+    amountContainer:{
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+    },
+    amountTitle:{
+        fontSize: 20,
+        fontWeight: 'bold',
+        padding: 10,
+    }
+
+
+})
