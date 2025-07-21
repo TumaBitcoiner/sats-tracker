effda3930c294b62f6cf4c5996d9dcdf8fc78ea4
TumaBitcoiner <tumathebitcoiner@gmail.com>
2025-07-21 17:10:04 +0200

    Modified cardTransaction to show amount+fee instead of amount only.

--- cards/cardTransaction.js
+++ cards/cardTransaction.js
@@ -28,7 +28,7 @@ export function CardTransaction({onPress, item}){
                             <Text style={item.isExpenses 
                                 ? globalStyles.transactionAmountExpense 
                                 : globalStyles.transactionAmountIncome}>
-                                {formatNumber(item.amount)} sats
+                                {formatNumber(item.amount + item.transactionFee)} sats
                             </Text>
                             <MaterialIcons 
                                 name={walletsArray.type[item.transactionType][0]}
