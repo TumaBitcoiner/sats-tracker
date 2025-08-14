469030c4e94cbde7d4f71765cf7fa119c72dfc5a
TumaBitcoiner <tumathebitcoiner@gmail.com>
2025-08-14 18:24:15 +0200

    Created basic screen for Budgets.

--- screens/budget.js
+++ screens/budget.js
@@ -1,7 +1,8 @@
-import React from 'react'
-import { StyleSheet, View, Text, TouchableOpacity } from 'react-native'
+import { StyleSheet, View, Text } from 'react-native'
 import { useNavigation } from "@react-navigation/native";
-import Card from '../shared/card'
+import { MaterialIcons } from '@expo/vector-icons'
+import ButtonCircular from '../shared/buttonCircular';
+import { globalStyles } from '../styles/global';
 
 
 export default function Budget(){
@@ -9,12 +10,33 @@ export default function Budget(){
     const navigation = useNavigation();
     
     return(
-        <View>
-            <TouchableOpacity onPress={()=> navigation.navigate('BudgetDetails')}>
-                <Card>
-                    <Text>Wait for it ;)</Text>
-                </Card>                
-            </TouchableOpacity>
+    
+        <View style={globalStyles.container}>
+
+            <View style={styles.emptyContainer}>
+                <MaterialIcons name='info' style={styles.emptyIcon} />
+                <Text style={styles.emptyText}>No budgets recorded for this wallet</Text>
+            </View>    
+            <ButtonCircular onPress={() => console.log("WEEE")} icon='add'/>
         </View>
     )    
 }
+
+const styles = StyleSheet.create({
+    emptyContainer: {   
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        padding: 20
+    },
+    emptyIcon: {
+        fontSize: 48,
+        color: '#666',
+        marginBottom: 16
+    },
+    emptyText: {
+        fontSize: 16,
+        color: '#666',
+        textAlign: 'center'
+    }
+});
