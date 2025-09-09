f7ea60277b04326cf07319b81c1a239228efc6b2
TumaBitcoiner <tumathebitcoiner@gmail.com>
2025-09-09 15:38:18 +0200

    Modified walletDetails screen;:     - Added tab navigation to move between Expenses and Income.     - Removed unused state variables and styles related to the what has been moved to SubWalletDetails.     - Created separate states and pie data for Expenses and Income.     - Adjusted styles for better layout with tabs.

--- screens/walletDetails.js
+++ screens/walletDetails.js
@@ -1,13 +1,10 @@
 import { useState, useEffect } from 'react';
-import {View, Text, FlatList, Dimensions, 
-    StyleSheet, Modal, TouchableWithoutFeedback, Keyboard } from 'react-native';
+import {View, Text, StyleSheet,
+     Modal, TouchableWithoutFeedback, Keyboard } from 'react-native';
 import { globalStyles } from '../styles/global';
 import { getCategoryTotalsByWallet, swapTransactions, consolidationTransaction } from '../database/database'; // Import the function to get totals
 import { useNavigation } from "@react-navigation/native";
-import { CardCategory } from '../cards/cardCategory';
-import { PieChart } from 'react-native-chart-kit';
-import Card from '../shared/card';
-import { EXPENSES_RANK_COLORS } from '../styles/categories';
+import { EXPENSES_RANK_COLORS, INCOME_RANK_COLORS } from '../styles/categories';
 import HeaderWithOptions from '../headers/headerWithOptions';
 import WalletOptions from '../modals/walletOptions';
 import SwapFunds from '../modals/walletOptionsActions/swapFunds';
@@ -15,12 +12,20 @@ import ConsolidateFunds from '../modals/walletOptionsActions/consolidateFunds';
 import WalletForm from '../modals/walletForm'; 
 import ConfirmationPopUp from '../modals/confirmationPopUp';
 import { MaterialIcons } from '@expo/vector-icons';
+import SubWalletDetails from './sub-screens/subWalletDetails';
+import { createMaterialTopTabNavigator } from '@react-navigation/material-top-tabs';
+
+const Tab = createMaterialTopTabNavigator();
+
 
 export default function WalletDetails({route}){
 
 
     const [walletExpenses, setWalletExpenses] = useState([]);
-    const [pieData, setPieData] = useState([]);
+    const [walletIncome, setWalletIncome] = useState([]);
+    const [pieDataExpenses, setPieDataExpenses] = useState([]);
+    const [pieDataIncome, setPieDataIncome] = useState([]);
+
 
     const [openWalletOptions, setOpenWalletOptions] = useState(false);
     const [openSwapModal, setOpenSwapModal] = useState(false);
@@ -28,40 +33,42 @@ export default function WalletDetails({route}){
     const [openEditWalletModal, setOpenEditWalletModal] = useState(false);
     const [openDeleteWalletModal, setOpenDeleteWalletModal] = useState(false);
 
-    //const [walletIncome, setWalletIncome] = useState([]);
-
     const navigation = useNavigation();
     
     // Transform wallet expenses into pie chart data
-    const createPieChartData = (expenses) => {
-        return expenses.map(expense => ({
-            name: expense.category,
-            amount: expense.totalSpent,
-            color: EXPENSES_RANK_COLORS[expense.category] || '#808080', // Random color
+    const createPieChartData = (transactionList, isExpense) => {
+
+        if (!transactionList || !Array.isArray(transactionList)) return [];
+
+        return transactionList.map(transaction => ({
+            name: transaction.category,
+            amount: transaction.totalSpent,
+            color: (isExpense ? EXPENSES_RANK_COLORS[transaction.category] : INCOME_RANK_COLORS[transaction.category]) || '#808080', // Random color
             legendFontColor: "#7F7F7F",
         }));
     };
-    
-    const renderItem = ({ item }) => (
-        <CardCategory
-            item={item}
-            onPress={() =>  console.log('Category pressed:', item.category)}
-            isExpenses={true}
-        />
-    );
-
-     const fetchTotalsOfCategoryByWallet = async () => {
+  
+    const fetchTotalsOfCategoryByWallet = async () => {
             
         try {
-                const totals = await getCategoryTotalsByWallet(route.params.id);
-                
-                setWalletExpenses(totals);
-                setPieData(createPieChartData(totals));
-               
-                console.log("Wallet Expenses:", totals);
-            } catch (error) {
-                console.error('Error fetching wallet details:', error);
-            }
+            const totalsExp = await getCategoryTotalsByWallet(route.params.id, true);
+            
+            setWalletExpenses(totalsExp);
+            console.log('Fetched wallet expenses:', totalsExp);
+
+            setPieDataExpenses(createPieChartData(totalsExp || [], true));
+
+            const totalsInc = await getCategoryTotalsByWallet(route.params.id, false);
+            setWalletIncome(totalsInc);
+
+            setPieDataIncome(createPieChartData(totalsInc || [], false));
+
+
+            console.log('Fetched wallet income:', totalsInc);
+
+        } catch (error) {
+            console.error('Error fetching wallet details:', error);
+        }
     };
 
     const manageWallet = (value) => {
@@ -94,15 +101,9 @@ export default function WalletDetails({route}){
     }
 
     useEffect(() => {
-
-        fetchTotalsOfCategoryByWallet();
-
+        //fetchTotalsOfCategoryByWallet();
         const unsubscribe = navigation.addListener('focus', fetchTotalsOfCategoryByWallet);
-        
-        // Cleanup subscription
-        return () => {
-            unsubscribe();
-        };
+        return () => unsubscribe();
     }, [navigation]);
 
     const handleSwapTransaction = async (values) => {
@@ -143,6 +144,7 @@ export default function WalletDetails({route}){
             console.error('Error editing wallet:', error);
         }
     }
+
     const handleDeleteWallet = async () => {
 
         setOpenDeleteWalletModal(false);
@@ -210,6 +212,7 @@ export default function WalletDetails({route}){
                     </View>
                 </TouchableWithoutFeedback>
             </Modal>
+
             {/* Delete wallet modal */}
             <Modal visible={openDeleteWalletModal} animationType="slide">
                 <TouchableWithoutFeedback onPress={() => setOpenDeleteWalletModal(false)}>
@@ -228,56 +231,58 @@ export default function WalletDetails({route}){
             </Modal>
 
             <HeaderWithOptions headerTitle='Wallet Details' navigation={navigation} onOptionPress={() => setOpenWalletOptions(true)}/>
-            { (walletExpenses.length === 0) ?
+            { (walletExpenses.length === 0 && walletIncome.length == 0) ?
                 <View style={styles.emptyContainer}>
                     <MaterialIcons name='info' style={styles.emptyIcon} />
-                    <Text style={styles.emptyText}>No expenses recorded for this wallet</Text>
+                    <Text style={styles.emptyText}>No transaction recorded for this wallet</Text>
                 </View>
                 :
-                <Card>
-                    <View style={globalStyles.cardContainer}>
-                        <Text style={globalStyles.titleText}>Expenses by Catergory</Text>
-                        <PieChart
-                            data={pieData}
-                            width={Dimensions.get("window").width - 40}
-                            height={220}
-                            chartConfig={{
-                                backgroundColor: "#ffffff",
-                                backgroundGradientFrom: "#ffffff",
-                                backgroundGradientTo: "#ffffff",
-                                color: (opacity = 1) => `rgba(0, 0, 0, ${opacity})`,
-                            }}
-                            accessor="amount"
-                            backgroundColor="transparent"
-                            paddingLeft="15"
-                        />
-                    </View>
-                </Card>
+                <Tab.Navigator
+                    screenOptions={{
+                        tabBarStyle: { backgroundColor: '#fff' },
+                        tabBarIndicatorStyle: { backgroundColor: 'orange' },
+                        tabBarLabelStyle: {
+                            textTransform: 'none',
+                            fontWeight: 'bold',
+                            fontSize: 18
+                        },
+                    }}
+                >
+                    <Tab.Screen 
+                        name="Expenses" 
+                        children={() => (
+                            walletExpenses.length === 0 ? (
+                                <View style={globalStyles.emptyContainer}>
+                                    <MaterialIcons name='info' style={globalStyles.emptyIcon} />
+                                    <Text style={globalStyles.emptyText}>No expense transaction added for this wallet.</Text>
+                                </View>
+                            ) : (
+                                <SubWalletDetails isExpense={true} walletTxs={walletExpenses} pieData={pieDataExpenses}/>
+                          
+                            )
+                        )}
+                    />
+                    <Tab.Screen 
+                        name="Income" 
+                        children={() => (
+                            walletIncome.length === 0 ? (
+                                <View style={globalStyles.emptyContainer}>
+                                    <MaterialIcons name='info' style={globalStyles.emptyIcon} />
+                                    <Text style={globalStyles.emptyText}>No income transaction added for this wallet.</Text>
+                                </View>
+                            ) : (
+                                <SubWalletDetails isExpense={false} walletTxs={walletIncome} pieData={pieDataIncome}/>
+                              
+                            )
+                        )}
+                    />
+                </Tab.Navigator>
             }
-            <View style={styles.listSection}>
-
-                <FlatList 
-                    data={walletExpenses}
-                    keyExtractor={item => item.category}
-                    renderItem={renderItem}
-                    contentContainerStyle={styles.listContent}
-                />
-
-            </View>
         </View>
     )
 }
 
 const styles = StyleSheet.create({
-    headerSection: {
-        paddingBottom: 10
-    },
-    listSection: {
-        flex: 1
-    },
-    listContent: {
-        paddingBottom: 50
-    },
     emptyContainer: {
         flex: 1,
         justifyContent: 'center',
