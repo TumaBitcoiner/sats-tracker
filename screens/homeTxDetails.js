8e717cc24631bc6f781dab9fd7a6b38a629ed0ce
TumaBitcoiner <tumathebitcoiner@gmail.com>
2025-09-29 15:42:45 +0200

    Added WIP message on homeTxDetails.

--- screens/homeTxDetails.js
+++ screens/homeTxDetails.js
@@ -3,18 +3,22 @@ import {StyleSheet, View, Text, Image} from 'react-native';
 import Card from "../shared/card";
 import HeaderWithOptions from "../headers/headerWithOptions";
 import { useNavigation } from "@react-navigation/native";
+import { globalStyles } from "../styles/global";
+import { MaterialIcons } from '@expo/vector-icons';
 
 export default function HomeTxDetails({route}){
 
     const navigation = useNavigation();
     
     return (
-    <View>
+    <View style={globalStyles.container}>
         <HeaderWithOptions headerTitle='Tx Details' navigation={navigation} onOptionPress={() => console.log*('BELLAA')}/>
         
-        <Card>
-            <Text>Home Tx details</Text>
-        </Card>
+        <View style={globalStyles.emptyContainer}>
+                <MaterialIcons name='build' style={globalStyles.emptyIcon} />
+                <Text style={globalStyles.emptyText}>Working on it!</Text>
+        </View>
+
     </View>
     )
 }
